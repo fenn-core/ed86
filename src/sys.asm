34ce30cedc8e5326888ff04e640282ce18ae0346
Arda Simsek <74985502+fenn-core@users.noreply.github.com>
2026-09-07 03:53:33 +0300

    implement file_open_ro

--- src/sys.asm
+++ src/sys.asm
@@ -6,24 +6,36 @@ global sys_exit
 
 section .text
 
+; ssize_t read(int fd, void *buf, size_t count);
 sys_read: 
 	xor eax, eax
 	syscall 
 	ret
 
 
+; int close(int fd);
 sys_write:
 	mov eax, 1
 	syscall
 	ret
 
 
+; int close(int fd);
 sys_close: 
 	mov eax, 3 
 	syscall
 	ret
-	
 
+
+; int openat(int dirfd, const char *path, int flags, mode_t mode);
+file_open_ro: 
+	mov eax, 257
+	mov r10, rcx
+	syscall
+	ret	
+
+
+; void exit(int status);
 sys_exit: 
 	mov eax, 60
 	syscall 
